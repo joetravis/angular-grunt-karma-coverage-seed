3475d7df46d2944d18a6ab28b6c97a356a13eefb
joetravis <joe.travis@adp.com>
2014-02-16 10:29:22 -0800

    Updating default tests

--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,12 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', ['ngResource'])
+    .value('version', '0.1')
+    .factory('myResourceService', ['$resource', function($resource) {
+        return {
+            getResource: function() {
+                return $resource;
+            }
+        };
+    }]);
