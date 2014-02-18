8a5650cd12dcded45506e650523210fd4dddca43
Craig Gardner <craig.s.gardner@gmail.com>
2014-02-17 17:56:05 -0800

    Initial commit for the maps

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,8 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['ngRoute', 'myApp.controllers', 'myApp.filters', 'myApp.services', 'myApp.directives']).
+angular.module('myApp', ['ngRoute', 'myApp.controllers', 'myApp.filters', 'myApp.services', 'myApp.directives', 'google-maps']).
     config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-        $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-        $routeProvider.otherwise({redirectTo: '/view1'});
+        $routeProvider.when('/map', {templateUrl: 'partials/map.html', controller: 'MapCtrl'});
+        $routeProvider.otherwise({redirectTo: '/map'});
     }]);
